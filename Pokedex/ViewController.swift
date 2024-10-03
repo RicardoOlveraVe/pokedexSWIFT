504ea17989be0e4cf90802d3f120c23b49622db6
Ricardo Olvera <140466264+RicardoOlveraVe@users.noreply.github.com>
2024-10-02 23:41:56 -0600

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Ricardo Olvera Velasco on 02/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
